e1af4a31cef6949c965cc931f574166cd2736da9
V16KMohamed29 <karim.othman-moahmed@vodafone.com>
2018-03-20 13:33:09 +0200

    Done For IPs and Date function

--- src/main/java/eg/com/vodafone/mi/export/ProductsExporter.java
+++ src/main/java/eg/com/vodafone/mi/export/ProductsExporter.java
@@ -17,125 +17,108 @@ import com.vaadin.server.VaadinService;
 
 import eg.com.vodafone.mi.sql.SQLStatement;
 
-public class ProductsExporter
-{
-    private static Logger logger = LoggerFactory.getLogger(ProductsExporter.class);
+public class ProductsExporter {
+	private static Logger logger = LoggerFactory.getLogger(ProductsExporter.class);
 
-    private String propertiesFile;
-    private JDBCConnectionPool sourceEnvCon;
-    private JDBCConnectionPool destEnvCon;
-    private boolean validateDestEnv;
+	private String propertiesFile;
+	private JDBCConnectionPool sourceEnvCon;
+	private JDBCConnectionPool destEnvCon;
+	private boolean validateDestEnv;
 
-    public ProductsExporter(String propertiesFile, JDBCConnectionPool sourceEnvCon, JDBCConnectionPool destEnvCon,
-	    boolean validateDestEnv)
-    {
-	this.propertiesFile = propertiesFile;
-	this.sourceEnvCon = sourceEnvCon;
-	this.destEnvCon = destEnvCon;
-	this.validateDestEnv = validateDestEnv;
-    }
-
-    public List<SQLStatement> export(String[] prdIDs)
-    {
-	List<SQLStatement> result = new ArrayList<SQLStatement>();
-
-	Properties prop = loadProperties();
-	String[] tables = prop.get("Tables").toString().split(",");
-	String[] tablesPrimaryKeys = prop.get("TablesPrimaryKeys").toString().split(",");
-	String[] tablesForeignKeys = prop.get("TablesForeignKeys").toString().split(",");
-	String[] relations = prop.get("Relations").toString().split(",");
-
-	TableExporter defTableExporter = new TableExporter(tables[0], tablesPrimaryKeys[0].split("\\.")[1],
-		getRelativesTables(tables[0], tablesPrimaryKeys, tablesForeignKeys, relations), sourceEnvCon);
-
-	RelativeTable relativeTable = new RelativeTable(relations[0], defTableExporter);
-
-	for (int i = 0; i < prdIDs.length; i++)
-	{
-	    result.addAll(relativeTable.exportAllRelevantSQLs(prdIDs[i]));
+	public ProductsExporter(String propertiesFile, JDBCConnectionPool sourceEnvCon, JDBCConnectionPool destEnvCon,
+			boolean validateDestEnv) {
+		this.propertiesFile = propertiesFile;
+		this.sourceEnvCon = sourceEnvCon;
+		this.destEnvCon = destEnvCon;
+		this.validateDestEnv = validateDestEnv;
 	}
 
-	if (validateDestEnv)
-	{
-	    logger.info("Checking against the destination envrionment..");
-	    return filterResult(result, tablesPrimaryKeys);
-	}
-	
-	return result;
-    }
+	public List<SQLStatement> export(String[] prdIDs) {
+		List<SQLStatement> result = new ArrayList<SQLStatement>();
 
-    private List<SQLStatement> filterResult(List<SQLStatement> result, String[] tablesPrimaryKeys)
-    {
-	List<SQLStatement> filteredResult = new ArrayList<SQLStatement>(result.size());
-	RowChecker rowChecker = new RowChecker(this.destEnvCon, tablesPrimaryKeys);
+		Properties prop = loadProperties();
+		String[] tables = prop.get("Tables").toString().split(",");
+		String[] tablesPrimaryKeys = prop.get("TablesPrimaryKeys").toString().split(",");
+		String[] tablesForeignKeys = prop.get("TablesForeignKeys").toString().split(",");
+		String[] relations = prop.get("Relations").toString().split(",");
 
-	for (SQLStatement sql : result)
-	{
-	    if (!rowChecker.isExists(sql))
-	    {
-		filteredResult.add(sql);
-	    }
-	}
+		TableExporter defTableExporter = new TableExporter(tables[0], tablesPrimaryKeys[0].split("\\.")[1],
+				getRelativesTables(tables[0], tablesPrimaryKeys, tablesForeignKeys, relations), sourceEnvCon);
 
-	return filteredResult;
-    }
+		RelativeTable relativeTable = new RelativeTable(relations[0], defTableExporter);
 
-    private RelativeTable[] getRelativesTables(String tableName, String[] tablesPrimaryKeys,
-	    String[] tablesForeignKeys, String[] relations)
-    {
-	List<RelativeTable> exporters = new ArrayList<RelativeTable>(relations.length);
+		for (int i = 0; i < prdIDs.length; i++) {
+			result.addAll(relativeTable.exportAllRelevantSQLs(prdIDs[i]));
+		}
 
-	for (int i = 0; i < relations.length; i++)
-	{
-	    if (relations[i].startsWith(tableName))
-	    {
-		String[] relation = relations[i].split("=");
-		String tableName2 = relation[1].split("\\.")[0];
+		if (validateDestEnv) {
+			logger.info("Checking against the destination envrionment..");
+			return filterResult(result, tablesPrimaryKeys);
+		}
 
-		logger.info(tableName2 + " is relative to " + tableName);
-
-		exporters.add(new RelativeTable(relations[i], new TableExporter(tableName2, getPrimaryKey(tableName2,
-			tablesPrimaryKeys), getRelativesTables(tableName2, tablesPrimaryKeys, tablesForeignKeys,
-			relations), sourceEnvCon)));
-	    }
+		return result;
 	}
 
-	RelativeTable[] relativeTables = new RelativeTable[exporters.size()];
-	exporters.toArray(relativeTables);
+	private List<SQLStatement> filterResult(List<SQLStatement> result, String[] tablesPrimaryKeys) {
+		List<SQLStatement> filteredResult = new ArrayList<SQLStatement>(result.size());
+		RowChecker rowChecker = new RowChecker(this.destEnvCon, tablesPrimaryKeys);
 
-	return relativeTables;
-    }
+		for (SQLStatement sql : result) {
+			if (!rowChecker.isExists(sql)) {
+				filteredResult.add(sql);
+			}
+		}
 
-    private String getPrimaryKey(String tableName, String[] tablesPrimaryKeys)
-    {
-	for (int i = 0; i < tablesPrimaryKeys.length; i++)
-	{
-	    if (tablesPrimaryKeys[i].startsWith(tableName))
-		return tablesPrimaryKeys[i].split("\\.")[1];
-	}
-	return null;
-    }
-
-    private Properties loadProperties()
-    {
-	Properties prop = new Properties();
-	try
-	{
-	    String basepath = VaadinService.getCurrent().getBaseDirectory().getAbsolutePath();
-	    prop.load(new FileInputStream(new File(basepath + "/WEB-INF/tables/" + propertiesFile)));
-	}
-	catch (IOException e)
-	{
-	    logger.error("Exception while reading the properties file..", e);
+		return filteredResult;
 	}
 
-	Set<Object> keySet = prop.keySet();
-	for (Iterator<Object> iterator = keySet.iterator(); iterator.hasNext();)
-	{
-	    Object object = iterator.next();
-	    logger.debug(object + "=" + prop.getProperty((String) object));
+	private RelativeTable[] getRelativesTables(String tableName, String[] tablesPrimaryKeys, String[] tablesForeignKeys,
+			String[] relations) {
+		List<RelativeTable> exporters = new ArrayList<RelativeTable>(relations.length);
+
+		for (int i = 0; i < relations.length; i++) {
+			if (relations[i].startsWith(tableName)) {
+				String[] relation = relations[i].split("=");
+				String tableName2 = relation[1].split("\\.")[0];
+
+				logger.info(tableName2 + " is relative to " + tableName);
+
+				exporters.add(new RelativeTable(relations[i],
+						new TableExporter(tableName2, getPrimaryKey(tableName2, tablesPrimaryKeys),
+								getRelativesTables(tableName2, tablesPrimaryKeys, tablesForeignKeys, relations),
+								sourceEnvCon)));
+			}
+		}
+
+		RelativeTable[] relativeTables = new RelativeTable[exporters.size()];
+		exporters.toArray(relativeTables);
+
+		return relativeTables;
 	}
 
-	return prop;
-    }
+	private String getPrimaryKey(String tableName, String[] tablesPrimaryKeys) {
+		for (int i = 0; i < tablesPrimaryKeys.length; i++) {
+			if (tablesPrimaryKeys[i].startsWith(tableName))
+				return tablesPrimaryKeys[i].split("\\.")[1];
+		}
+		return null;
+	}
+
+	private Properties loadProperties() {
+		Properties prop = new Properties();
+		try {
+			String basepath = VaadinService.getCurrent().getBaseDirectory().getAbsolutePath();
+			prop.load(new FileInputStream(new File(basepath + "/WEB-INF/tables/" + propertiesFile)));
+		} catch (IOException e) {
+			logger.error("Exception while reading the properties file..", e);
+		}
+
+		Set<Object> keySet = prop.keySet();
+		for (Iterator<Object> iterator = keySet.iterator(); iterator.hasNext();) {
+			Object object = iterator.next();
+			logger.debug(object + "=" + prop.getProperty((String) object));
+		}
+
+		return prop;
+	}
 }
